95d3401558ab043ab53a0c9b5c93d9a335ecf200
trswkz <blueink_49@mac.com>
2020-09-15 02:36:58 +0900

    Source Struct

--- Sources/SourceKit/Renderable/SourceRenderable.swift
+++ Sources/SourceKit/Renderable/SourceRenderable.swift
@@ -3,3 +3,10 @@ import Foundation
 public protocol SourceRenderable {
     var source: String { get }
 }
+
+public struct Source {
+    public let source: String
+    public init(@SourceBuilder _ source: () -> String){
+        self.source = source()
+    }
+}
