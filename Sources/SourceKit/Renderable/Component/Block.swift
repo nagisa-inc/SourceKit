c018acb8706258414cb5fdaeaf76523d68cbcb29
trswkz <blueink_49@mac.com>
2020-09-15 12:30:28 +0900

    [feature] BlockCommenr

--- Sources/SourceKit/Renderable/Component/Block.swift
+++ Sources/SourceKit/Renderable/Component/Block.swift
@@ -1,13 +1,19 @@
 import Foundation
 
-public struct Block: SourceRenderable {
+public class Block: SourceRenderable {
     public let source: String
 
-    public init(@SourceBuilder _ head: ()->String, @SourceBuilder contents: ()->String) {
-        source = "\(head()){\n\(contents())\n}"
+    public init(@SourceBuilder _ head: ()->String, open: String = "{", close: String = "}", @SourceBuilder contents: ()->String) {
+        source = "\(head())\(open)\n\(contents())\n\(close)"
     }
 
-    public init(_ head: String, @SourceBuilder contents: ()->String) {
-        source = "\(head){\n\(contents())\n}"
+    public init(_ head: String, open: String = "{", close: String = "}", @SourceBuilder contents: ()->String) {
+        source = "\(head)\(open)\n\(contents())\n\(close)"
+    }
+}
+
+public class BlockComment: Block {
+    public override init(_ head: String, open: String = "/** ", close: String = "**/", contents: () -> String) {
+        super.init(head, open: open, close: close, contents: contents)
     }
 }
