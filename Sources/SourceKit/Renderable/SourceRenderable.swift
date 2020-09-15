1adf47ad089c6a0b151b2ca1a263ee5626f4c7da
trswkz <blueink_49@mac.com>
2020-09-15 11:52:20 +0900

    [fix] Source confirmerance

--- Sources/SourceKit/Renderable/SourceRenderable.swift
+++ Sources/SourceKit/Renderable/SourceRenderable.swift
@@ -4,7 +4,7 @@ public protocol SourceRenderable {
     var source: String { get }
 }
 
-public struct Source {
+public struct Source: SourceRenderable {
     public let source: String
     public init(@SourceBuilder _ source: () -> String){
         self.source = source()
