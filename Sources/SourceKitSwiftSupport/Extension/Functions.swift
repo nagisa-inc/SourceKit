cbdc50d0fba8129c33bebbbe93efac51d784cef4
trswkz <blueink_49@mac.com>
2020-10-23 03:43:26 +0900

    fix

--- Sources/SourceKitSwiftSupport/Extension/Functions.swift
+++ Sources/SourceKitSwiftSupport/Extension/Functions.swift
@@ -7,9 +7,15 @@ public class Func: SourceRenderable {
                 members: [Member] = [],
                 @SourceBuilder _ body: ()->SourceRenderable){
         self.source = Block({
-            Block("\(acl.rawValue) func \(name)", bracket: .round){
+            Block("\(acl.rawValue) init", bracket: .round){
                 ForIn(members, separator: ","){ member in
-                    "\(member.name): \(member.type)\(member.optional ? "?" : "")\(member.defaultValue.map({ "= \($0)" }) ?? "")"
+                    var result = "\(member.name): \(member.type)\(member.optional ? "?" : "")"
+                    if member.optional && member.defaultNil {
+                        result += "= nil"
+                    } else if let defaultValue = member.defaultValue {
+                        result += "= \(defaultValue)"
+                    }
+                    return result
                 }
             }
         }){}.source
